08df7af2b540acbe118cc9647a6db3dd1b5f8c83
908jyw <908jyw@gmail.com>
2020-07-02 18:07:50 +0900

    first commit

---
+++ src/algorithm/StartTaxi_baeckjoon_19238.java
@@ -0,0 +1,11 @@
+package algorithm;
+
+public class StartTaxi_baeckjoon_19238 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Java");
+	}
+
+}
