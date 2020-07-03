8039999ab548a10db805fb0ff735a2de9d692419
908jyw <908jyw@gmail.com>
2020-07-03 10:43:02 +0900

    test2 starttaxi 변경

--- src/baeckjooon/StartTaxi_19238.java
+++ src/baeckjooon/StartTaxi_19238.java
@@ -5,12 +5,7 @@ public class StartTaxi_19238 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello Java");
-		
-		System.out.println("테스트 브랜치에 추가");
-		
-		System.out.println("테스트 브랜치에 두번째 추가");
-		
+		System.out.println("test2 starttaxi 변경");
 	}
 
 }
